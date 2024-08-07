1de7c9afc76b8bc14abcdf52499c19a5f9e57ee6
L_Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2024-08-07 17:34:41 -0300

    Migração para Typescript e criação de interface
    Criamos a interfaces para cada componente e com isso migramos os componentes "Botao, Campo, Colaborador, Fomulario, ListaSuspensa e Time"

--- src/componentes/Botao/index.tsx
+++ src/componentes/Botao/index.tsx
@@ -1,14 +1,13 @@
 import { ReactElement } from 'react'
 import './Botao.css'
-import React from 'react';
 
 interface BotaoProps {
-  children: ReactElement
+  children: ReactElement | string
 }
 
-const Botao = (props: BotaoProps) => {
+const Botao = ({children}: BotaoProps) => {
   return (<button className='botao'>
-    {props.children}
+    {children}
   </button>)
 }
 
